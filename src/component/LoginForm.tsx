a4634852ff7f12c92087a87f4d2148be01df1640
Ryungeon Hong <ekdlwp123@gmail.com>
2024-03-18 15:03:01 +0900

    로그인 form 변경

--- src/component/LoginForm.tsx
+++ src/component/LoginForm.tsx
@@ -1,45 +1,52 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from './Button';
+import { Link } from 'react-router-dom';
 interface LoginFormProps {
   onLogin: (email: string, password: string) => void;
-  loading:boolean;
-  error:string;
+  loading: boolean;
+  error: string;
 }
 
-const LoginForm: React.FC<LoginFormProps> = ({ onLogin,loading,error }) => {
+const LoginForm: React.FC<LoginFormProps> = ({ onLogin, loading, error }) => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLoginClick = () => {
+  // 폼 제출 처리
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // 폼 제출 시 페이지 리로드 방지
     onLogin(email, password);
   };
-  const handleSignupClick = () => {
-    return  navigate("/signup")
-  }
+
   return (
     <div className='input-layout'>
-      <div className='login-form'>
-      <h2 className='login-form__title'>로그인</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        className='login-form__input'
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        className='login-form__input'
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <span className='login-form__sign-up-button' onClick={handleSignupClick}>회원가입</span>
-      <Button isLoading={loading} fullWidth={true} label='로그인' onClick={handleLoginClick}/>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      </div>
+      <form className='login-form' onSubmit={handleFormSubmit}>
+        <h2 className='login-form__title'>로그인</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          className='login-form__input'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className='login-form__input'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className='login-form__sign-up-button'>
+          <Link to={"/signup"}>회원가입</Link>
+        </div>
+        <Button isLoading={loading} fullWidth={true} label='로그인' />
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </form>
     </div>
   );
 };
 
 export default LoginForm;
+
+
